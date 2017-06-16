f439ab9342cfe43b5a980dc66dc7db4830b1cd5a
Adam Szechy <szech713@gmail.com>
2017-06-15 22:15:09 -0400

    openxc integration

--- app/src/main/java/com/amsengineeringllc/dash/Activity_starter.java
+++ app/src/main/java/com/amsengineeringllc/dash/Activity_starter.java
@@ -2,15 +2,24 @@ package com.amsengineeringllc.dash;
 
 import android.animation.Animator;
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.openxc.VehicleManager;
+import com.openxc.measurements.EngineSpeed;
+import com.openxc.measurements.Measurement;
 import com.sccomponents.widgets.ScArcGauge;
 import com.sccomponents.widgets.ScCopier;
 import com.sccomponents.widgets.ScGauge;
@@ -28,6 +37,11 @@ import java.util.Random;
  * status bar and navigation/system bar) with user interaction.
  */
 public class Activity_starter extends AppCompatActivity {
+    private static final String TAG = "Activity_starter";
+
+    private VehicleManager mVehicleManager;
+    private TextView mEngineSpeedView;
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -103,6 +117,8 @@ public class Activity_starter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_starter);
+
+        mEngineSpeedView = (TextView) findViewById(R.id.counter);
 
         // Find the components
         final ScArcGauge gauge = (ScArcGauge) this.findViewById(R.id.gauge);
@@ -201,6 +217,91 @@ public class Activity_starter extends AppCompatActivity {
         // are available.
         delayedHide(100);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // When the activity goes into the background or exits, we want to make
+        // sure to unbind from the service to avoid leaking memory
+        if(mVehicleManager != null) {
+            Log.i(TAG, "Unbinding from Vehicle Manager");
+            // Remember to remove your listeners, in typical Android
+            // fashion.
+            mVehicleManager.removeListener(EngineSpeed.class,
+                    mSpeedListener);
+            unbindService(mConnection);
+            mVehicleManager = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // When the activity starts up or returns from the background,
+        // re-connect to the VehicleManager so we can receive updates.
+        if(mVehicleManager == null) {
+            Intent intent = new Intent(this, VehicleManager.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    /* This is an OpenXC measurement listener object - the type is recognized
+     * by the VehicleManager as something that can receive measurement updates.
+     * Later in the file, we'll ask the VehicleManager to call the receive()
+     * function here whenever a new EngineSpeed value arrives.
+     */
+    EngineSpeed.Listener mSpeedListener = new EngineSpeed.Listener() {
+        @Override
+        public void receive(Measurement measurement) {
+            // When we receive a new EngineSpeed value from the car, we want to
+            // update the UI to display the new value. First we cast the generic
+            // Measurement back to the type we know it to be, an EngineSpeed.
+            final EngineSpeed speed = (EngineSpeed) measurement;
+            // In order to modify the UI, we have to make sure the code is
+            // running on the "UI thread" - Google around for this, it's an
+            // important concept in Android.
+            Activity_starter.this.runOnUiThread(new Runnable() {
+                public void run() {
+                    // Finally, we've got a new value and we're running on the
+                    // UI thread - we set the text of the EngineSpeed view to
+                    // the latest value
+                    mEngineSpeedView.setText("Engine speed (RPM): "
+                            + speed.getValue().doubleValue());
+                }
+            });
+        }
+    };
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        // Called when the connection with the VehicleManager service is
+        // established, i.e. bound.
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            Log.i(TAG, "Bound to VehicleManager");
+            // When the VehicleManager starts up, we store a reference to it
+            // here in "mVehicleManager" so we can call functions on it
+            // elsewhere in our code.
+            mVehicleManager = ((VehicleManager.VehicleBinder) service)
+                    .getService();
+
+            // We want to receive updates whenever the EngineSpeed changes. We
+            // have an EngineSpeed.Listener (see above, mSpeedListener) and here
+            // we request that the VehicleManager call its receive() method
+            // whenever the EngineSpeed changes
+            mVehicleManager.addListener(EngineSpeed.class, mSpeedListener);
+        }
+
+        // Called when the connection with the service disconnects unexpectedly
+        public void onServiceDisconnected(ComponentName className) {
+            Log.w(TAG, "VehicleManager Service  disconnected unexpectedly");
+            mVehicleManager = null;
+        }
+    };
+
+
+
+
+
 
 
     private void toggle() {
